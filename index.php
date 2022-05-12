ded834ff4887f21661dd9c95d439e2ee9f2d708a
unknown <fabio_pachecosilva@hotmail.com>
2022-05-12 18:25:39 -0300

    adicionando texto sus

--- index.php
+++ index.php
@@ -518,7 +518,7 @@
 		<h1 class="">INGRESSOS</h1>
 
 		<p class="aviso">
-			Atendendo aos decretos municipais n°60.488 (São Paulo), o evento Jurassic Safari Experience, solicitará ao público que irá assistir e viver a experiência , o comprovante de vacinação do cidadão contra a COVID-19 para o acesso aos eventos. O público poderá apresentar o comprovante físico de vacinação (carteirinha de vacinação), ou de forma digital disponível nas plataformas ConectSUS, que podem ser baixados na loja de aplicativos do seu smartphone.
+			Atendendo aos decretos municipais n°60.488 (São Paulo), o evento Jurassic Safari Experience, solicitará ao público que irá assistir e viver a experiência , o comprovante de vacinação do cidadão contra a COVID-19 para o acesso aos eventos. O público poderá apresentar o comprovante físico de vacinação (carteirinha de vacinação), ou de forma digital disponível nas plataformas ConectSUS, que podem ser baixados na loja de aplicativos do seu smartphone. Condições sujeitas às mudanças dos órgãos reguladores até a data de sua sessão.
 		</p>
 
 		<div class="wrapper-card">			
